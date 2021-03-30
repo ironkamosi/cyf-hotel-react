bd88151c524bb9a065309022c77f2e96683f649f
ironkamosi <alintonchambers@gmail.com>
2021-03-30 02:10:49 +0200

    completed exercise 21

--- src/CustomerProfile.js
+++ src/CustomerProfile.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CustomerProfile = props => {
+  const [clientEmail, setClientEmail] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [vip, setVip] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  //   console.log(` fetch cp ${props.clientId}`);
+
+  useEffect(() => {
+    fetch(`https://cyf-react.glitch.me/customers/${props.clientId}`)
+      .then(result => result.json())
+      .then(data => {
+        console.log("fetch", data);
+        setClientId(data.id);
+        setClientEmail(data.email);
+        setVip(data.vip);
+        setPhoneNumber(data.phoneNumber);
+      });
+  }, [props.clientId]); // Always remember to put an empty array here!
+
   // console.log("Customer props", props)
-  return <p>{props.clientId}</p>;
+  return (
+    <div>
+      <p>{clientId}</p>
+      <p>{clientEmail}</p>
+      {vip ? <p>VIP</p> : null}
+      <p>{phoneNumber}</p>
+    </div>
+  );
 };
 
 export default CustomerProfile;
